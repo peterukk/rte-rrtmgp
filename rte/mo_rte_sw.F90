a629a5ba5046c6c5933f1792229d9f1be74ae22e
Peter Ukkonen <peterukk@gmail.com>
2021-02-02 11:45:17 +0100

    rte_config is now rte_rrtmgp_config and includes configuration of RRTMGP-NN
    Former-commit-id: f0b0aedcc6c29f08b08dc4ebcb2afb2c82b17b51

--- rte/mo_rte_sw.F90
+++ rte/mo_rte_sw.F90
@@ -28,14 +28,14 @@
 !
 ! -------------------------------------------------------------------------------------------------
 module mo_rte_sw
-  use mo_rte_kind,      only: wp, wl
-  use mo_rte_config,    only: check_extents, check_values
-  use mo_rte_util_array,only: any_vals_less_than, any_vals_outside, extents_are
-  use mo_optical_props, only: ty_optical_props, &
+  use mo_rte_kind,          only: wp, wl
+  use mo_rte_rrtmgp_config, only: check_extents, check_values
+  use mo_rte_util_array,    only: any_vals_less_than, any_vals_outside, extents_are
+  use mo_optical_props,     only: ty_optical_props, &
                               ty_optical_props_arry, ty_optical_props_1scl, ty_optical_props_2str, ty_optical_props_nstr
-  use mo_fluxes,        only: ty_fluxes, ty_fluxes_broadband
+  use mo_fluxes,            only: ty_fluxes, ty_fluxes_broadband
   use mo_rte_solver_kernels, &
-                        only: apply_BC, sw_solver_noscat, sw_solver_2stream, sw_solver_noscat_broadband, sw_solver_2stream_broadband
+                            only: apply_BC, sw_solver_noscat, sw_solver_2stream, sw_solver_noscat_broadband, sw_solver_2stream_broadband
   use mo_fluxes_broadband_kernels, only : sum_broadband, sum_broadband_nocol
 #ifdef USE_TIMING
   !
